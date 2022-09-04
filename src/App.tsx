471ac370603f8b472a8fcf79d162219ddb61cdcb
chasemc67 <chasemc67@gmail.com>
2022-09-04 16:33:29 -0700

    Render better name for button

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,6 @@ export function App() {
   const rootNodes = useSelector((state: RootState) => state.Trees.nodes);
   // const dispatch = useDispatch();
 
-  let renderButton = function (button: any) {
-    return (
-      <div>
-        <button>{button.name}</button>
-        <div className="children"></div>
-        {button.children.map((child: any) => {
-          return renderButton(child);
-        })}
-      </div>
-    );
-  };
-
   let onButtonClick = function (fileName: string, lineNumber?: number) {
     // @ts-ignore
     const vscode = VSCodeApiGetter.getVsCodeApi(); // this needs to be moved to some global singleton or something
@@ -36,7 +24,7 @@ export function App() {
       <header className="App-header">
         <h1 className="App-title">Welcome to Tree Tabs Hooks</h1>
         {rootNodes.map((node) => {
-          let name = `${node.fileName}:${
+          let name = `${node.fileName.split("/").pop()}:${
             node.lineNumber !== undefined && node.lineNumber + 1
           }`; // add 1 to lineNumber to match UI gutter because they're 0-indexed internally
           return (
