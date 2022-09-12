af8cf7e8a6b5b7d6d47bbba979b313d77ceebb76
chasemc67 <chasemc67@gmail.com>
2022-09-11 19:16:51 -0700

    integrated new code in, ready to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
 import React from "react";
-import type { RootState } from "./ReduxStore";
-import { useSelector, useDispatch } from "react-redux";
-import { VSCodeApiGetter } from "./VSCodeApiGetter";
 
 import "./App.css";
+import TreeTabs from "./TreeTabs/TreeTabs";
 
 export function App() {
-  const rootNodes = useSelector((state: RootState) => state.Trees.nodes);
-  // const dispatch = useDispatch();
-
-  let onButtonClick = function (fileName: string, lineNumber?: number) {
-    // @ts-ignore
-    const vscode = VSCodeApiGetter.getVsCodeApi(); // this needs to be moved to some global singleton or something
-    vscode.postMessage({
-      command: "tabSelected",
-      fileName,
-      lineNumber,
-    });
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to Tree Tabs Hooks</h1>
-        {rootNodes.map((node) => {
-          let name = `${node.fileName.split("/").pop()}:${
-            node.lineNumber !== undefined && node.lineNumber + 1
-          }`; // add 1 to lineNumber to match UI gutter because they're 0-indexed internally
-          return (
-            <button
-              aria-label={name}
-              onClick={() => onButtonClick(node.fileName, node.lineNumber)}
-            >
-              {name}
-            </button>
-          );
-        })}
-      </header>
+      <TreeTabs />
     </div>
   );
 }
---
+++ src/TreeTabs/TreeTabs.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import type { RootState } from "../ReduxStore";
+import { useSelector, useDispatch } from "react-redux";
+import { VSCodeApiGetter } from "../VSCodeApiGetter";
+import "./TreeTabs.css";
+
+function Tab(props: { name: string; closeTab: any; onClick: any }) {
+  const [elPosition, setElPosition] = useState({ top: 5, left: 5 });
+  const [hasMoved, setHasMoved] = useState(false);
+  let cursorOffset = {
+    x: 0,
+    y: 0,
+  };
+
+  const handleDragStart = (e: any) => {
+    cursorOffset = {
+      x: e.clientX - e.currentTarget.getBoundingClientRect().left,
+      y: e.clientY - e.currentTarget.getBoundingClientRect().top,
+    };
+
+    // set CSS
+    e.currentTarget.style.opacity = 0.3;
+
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const handleDragEnd = (e: any) => {
+    e.preventDefault();
+    e.currentTarget.style.opacity = 1;
+    setElPosition({
+      left: e.clientX - cursorOffset.x,
+      top: e.clientY - cursorOffset.y,
+    });
+    setHasMoved(true);
+  };
+
+  const handleDagOver = (e: any) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+  };
+
+  const getStyle = () => {
+    return elPosition;
+  };
+
+  return (
+    <div
+      className={hasMoved ? "tree-tab" : "tree-tab-new"}
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDrop={handleDrop}
+      onDragOver={handleDagOver}
+      style={getStyle()}
+    >
+      <button onClick={props.onClick}>{props.name}</button>
+      <button
+        className="tab-close-button"
+        onClick={(e) => props.closeTab(props.name)}
+      >
+        X
+      </button>
+    </div>
+  );
+}
+
+function TreeTabs() {
+  const rootNodes = useSelector((state: RootState) => state.Trees.nodes);
+
+  let onButtonClick = function (fileName: string, lineNumber?: number) {
+    // @ts-ignore
+    const vscode = VSCodeApiGetter.getVsCodeApi();
+    vscode.postMessage({
+      command: "tabSelected",
+      fileName,
+      lineNumber,
+    });
+  };
+
+  const closeTab = (tabName: string) => {
+    // setTabs(tabs.filter((t) => t !== tabName));
+  };
+
+  const getNameForNode = (node: any) => {
+    return `${node.fileName.split("/").pop()}:${
+      node.lineNumber !== undefined && node.lineNumber + 1
+    }`;
+  };
+
+  const getKeyForNode = (node: any) => {
+    return getNameForNode(node);
+  };
+
+  return (
+    <div>
+      <div
+        className="tree-tabs-root"
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {rootNodes.map((node) => (
+          <Tab
+            key={getKeyForNode(node)}
+            name={getNameForNode(node)}
+            onClick={() => onButtonClick(node.fileName, node.lineNumber)}
+            closeTab={closeTab}
+          ></Tab>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TreeTabs;
